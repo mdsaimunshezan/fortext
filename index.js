b1fdaa58b2920c8cfc9b12d62a36a9ee14efdf60
saimun islma <saimunshezan@gmail.com>
2022-12-14 23:53:23 +0600

    second

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 5050;
 
+app.get("/home", (req, res) => {
+    res.send('working....')
+})
+
 app.listen(port, () => {
     console.log("server is raning on the port is 5050");
 })
